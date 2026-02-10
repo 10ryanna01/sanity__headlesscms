3ba8eb300f0c2b511624fb2dd68ec40691f78eb6
Ryann Al-lahiq <ryann.allahiq@gmail.com>
2026-02-10 11:23:05 +0000

    updated deployhook

--- sanity.config.js
+++ sanity.config.js
@@ -3,6 +3,7 @@ import { structureTool } from "sanity/structure";
 import { defineConfig } from "sanity";
 import schemas from "./schemas/schema";
 import { netlifyTool } from "sanity-plugin-netlify";
+
 export default defineConfig({
   title: "timelineportfolio",
   id: "schema-experiment",
