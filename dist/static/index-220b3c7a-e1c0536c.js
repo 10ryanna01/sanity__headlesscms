1ab48de52b5cb2cec6fc19d10797844d03d59b00
Ryann Al-lahiq <ryann.allahiq@gmail.com>
2026-04-22 07:05:50 +0100

    updated and cleaned up

--- dist/static/index-220b3c7a-e1c0536c.js
+++
@@ -1,1 +0,0 @@
-import{DocumentPaneProvider as t,DocumentPane as a}from"./index-98d70598-bc7eb224.js";import"./sanity-ae81e8a1.js";export{t as DocumentPaneProvider,a as default};
---
+++ public/static/sanity-BTMrrTEI.js
@@ -0,0 +1,1 @@
+import{r as t,s as e}from"./sanity.config-fcQ4d030.js";t(document.getElementById("sanity"),e,{reactStrictMode:!1,basePath:"/"});
